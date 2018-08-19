6b92473be9d74869b51e17abe453d0c84fe4537a
Tomohiro Ueno <b1400500@planet.kanazawa-it.ac.jp>
2018-08-19 14:08:49 +0900

    epsilon parameter value changed

--- src/ddqn_rl_zoo/ddqn_5actions.py
+++ src/ddqn_rl_zoo/ddqn_5actions.py
@@ -32,7 +32,7 @@ class DoubleDQNAgent:
         self.learning_rate = 0.001
         self.epsilon = 1.0
         self.epsilon_decay = 0.99999
-        self.epsilon_min = 0.01
+        self.epsilon_min = 0.1
         self.batch_size = 64
         self.train_start = 1000
         self.buy_sell_len=2
